6e7a380b1c83710a164fd37546f627632d33c5dc
zapolnoch <zapolnoch@gmail.com>
2021-04-12 15:34:33 +0300

    step-2

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import { observer } from "mobx-react"
 import { counterStore } from "./counter.store"
 
+const TestComp = () => {
+  console.log("render", new Date())
+  return <div>test</div>
+}
+
 const App = observer(() => {
   return (
     <div className="App">
       <h1>{counterStore.count}</h1>
+      <TestComp />
       <h2>{counterStore.double}</h2>
 
       <button onClick={counterStore.inc}>+</button>
